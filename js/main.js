269a3c1c1c3372b0aea97558f874700c54e93257
Ashley Thompson <ashley.thompson769@gmail.com>
2018-06-09 23:48:59 -0400

    Unit 9 - moved logic into functions

--- js/main.js
+++ js/main.js
@@ -2,18 +2,23 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You Found a match!");
     } else {
         alert("Sorry, try again");
     }
 }
+
+function flipCard(cardId) {
+    console.log("User flipped " + cards[cardId]);
+
+    cardsInPlay.push(cards[cardId]);
+
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+}
+
+flipCard(2);
+flipCard(3);
